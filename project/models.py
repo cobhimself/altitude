8626986c1147344a50886dd1e038768a525a9d96
Collin Brooks <collin.brooks@gmail.com>
2011-07-11 00:14:55 -0500

    Working on getting the project model setup. Needs more work.

--- project/models.py
+++ project/models.py
@@ -1,14 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
+from altitude.accounts import Department
 
 # Create your models here.
-class Task(models.Model):
-	name = models.CharField(max_length = 140)
-	description = models.TextField()
-	due_date = models.DateField()
-	asignees = models.CommaSeparatedIntegerField(max_length = 100)
-	dependancies = models.CommaSeparatedIntegerField(max_length = 100)
 
 class Project(models.Model):
 	name = models.CharField(max_length = 100)
+    acronym = models.CharField(max_length = 5)
 	due_date = models.DateField()
 	
+class Milestone(models.Model):
+    name = models.CharField(max_length = 140)
+    description = models.TextField()
+    due_date = models.DateField()
+    project = models.ForeignKey('Project')
+
+class Element(models.Model):
+    name = models.CharField(max_length = 140)
+    project = models.ForeignKey('Project')
+    order = models.IntegerField()
+    percentComplete = models.DecimalField(max_digits=5, decimal_places=2)
+
+class DepartmentTask(models.Model):
+    parentElement = models.ForeignKey(Element)
+    department = models.ForeignKey(Department)
+
+class Task(models.Model):
+	name = models.CharField(max_length = 140)
+    project = models.ForeignKey('Project')
+	description = models.TextField()
+	due_date = models.DateField()
+	asignees = models.ManyToManyField(User)
+	dependancies = models.ManyToManyField('self')
+
